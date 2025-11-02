d4d687c0cf47e3210cc3163b9b7376e50ba48003
mugisham37 <mugisham333@gmail.com>
2025-11-02 21:06:58 +0200

    Removed the unwanted documents

--- apps/web-app/src/components/github-syncer-widget/index.ts
+++
@@ -1,6 +0,0 @@
-export { GithubSyncerWidget } from './GithubSyncerWidget'
-export { ActiveAutomaticSync } from './ActiveAutomaticSync'
-export { ActiveManualSync } from './ActiveManualSync'
-export { MiniAdvert } from './MiniAdvert'
-export { PausedSync } from './PausedSync'
-export type { GithubSyncerWidgetProps, SyncObj } from './GithubSyncerWidget'
--- apps/web-app/src/components/partner/index.ts
+++
@@ -1,2 +0,0 @@
-export { PartnerAdvert } from './PartnerAdvert';
-export { PartnerPerk } from './PartnerPerk';
