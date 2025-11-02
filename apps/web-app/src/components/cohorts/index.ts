8f20c0664abc7d6f2fe80d398fa86cf01f852cad
mugisham37 <mugisham333@gmail.com>
2025-11-02 19:25:29 +0200

    Removed unwanted documents

--- apps/web-app/src/components/cohorts/index.ts
+++
@@ -1,4 +0,0 @@
-export { CohortPage } from './CohortPage'
-export { GohortContent } from './GohortContent'
-export { ExhortContent } from './ExhortContent'
-export { CohortRegistrationForm } from './CohortRegistrationForm'
