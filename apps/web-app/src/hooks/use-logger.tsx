13d6cf686ba51b0baaada74078c2e7b28e25b690
mugisham37 <mugisham333@gmail.com>
2025-11-03 13:16:07 +0200

    Removed the unwanted documents

--- apps/web-app/src/hooks/use-logger.tsx
+++
@@ -1,9 +0,0 @@
-
-import { useEffect } from 'react'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useLogger(label: string, data: any): void {
-  useEffect(() => {
-    console.log(label, data)
-  }, [label, data])
-}
